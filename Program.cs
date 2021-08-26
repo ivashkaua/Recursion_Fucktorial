bd6843cf2a92135a56369e6c371ca1feb2dcbdde
ivashkaua <86633081+ivashkaua@users.noreply.github.com>
2021-08-27 01:22:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static int Fucktorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Fucktorial(x - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input the number:");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Result of {x}! is " + Fucktorial(x));
+                Console.ReadLine();
+                Console.Clear();
+                
+            }
+        }
+    }
+}
